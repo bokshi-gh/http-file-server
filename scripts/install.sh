c5469e556d7f49018096316559c194172e50f50e
Rajesh Thapa <devrajeshthapa@gmail.com>
2026-07-05 22:02:12 +0545

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e  # Exit immediately if a command fails
 
-REPO="https://github.com/bokshi-gh/file-server.git"
-TMP_DIR="/tmp/file-server-build"
-EXEC_NAME="goserve"
+REPO="https://github.com/bokshi-gh/http-file-server.git"
+TMP_DIR="/tmp/http-file-server-build"
+EXEC_NAME="httpfs"
 
 rm -rf "$TMP_DIR"
 
 git clone "$REPO" "$TMP_DIR"
 
-cd "$TMP_DIR/cmd/goserve"
+cd "$TMP_DIR/cmd/httpfs"
 go build -o "$EXEC_NAME"
 
 sudo mv "$EXEC_NAME" /usr/local/bin/
